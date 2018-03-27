73539e726b839d356202be3ad92d09e86c54f12e
Bradley Cicenas <bradley@vektor.nyc>
2018-03-27 02:04:50 +0000

    fix typo

--- README.md
+++ README.md
@@ -17,15 +17,15 @@ import (
 )
 
 func main() {
-	// convert a simple float from celsius to farenheit
-	fmt.Println(u.MustConvertFloat(25.5, u.Celsius, u.Farenheit)) // outputs "77.9 farenheit"
+	// convert a simple float from celsius to fahrenheit
+	fmt.Println(u.MustConvertFloat(25.5, u.Celsius, u.Fahrenheit)) // outputs "77.9 fahrenheit"
 
 	// convert a units.Value instance
 	val := u.NewValue(25.5, u.Celsius)
 
-	fmt.Println(val)                          // "25.5 celsius"
-	fmt.Println(val.MustConvert(u.Farenheit)) // 77.9 farenheit
-	fmt.Println(val.MustConvert(u.Kelvin))    // 298.65 kelvin
+	fmt.Println(val)                           // "25.5 celsius"
+	fmt.Println(val.MustConvert(u.Fahrenheit)) // "77.9 fahrenheit"
+	fmt.Println(val.MustConvert(u.Kelvin))     // "298.65 kelvin"
 }
 ```
 
--- temp_units.go
+++ temp_units.go
@@ -3,14 +3,14 @@ package units
 var (
 	Temp = UnitOptionQuantity("temperature")
 
-	Celsius   = NewUnit("celsius", "C", Temp, UnitOptionPlural("none"), SI)
-	Farenheit = NewUnit("farenheit", "F", Temp, UnitOptionPlural("none"), US)
-	Kelvin    = NewUnit("kelvin", "K", Temp, UnitOptionPlural("none"), SI)
+	Celsius    = NewUnit("celsius", "C", Temp, UnitOptionPlural("none"), SI)
+	Fahrenheit = NewUnit("fahrenheit", "F", Temp, UnitOptionPlural("none"), US)
+	Kelvin     = NewUnit("kelvin", "K", Temp, UnitOptionPlural("none"), SI)
 )
 
 func init() {
-	NewConversion(Celsius, Farenheit, "x * 1.8 + 32")
-	NewConversion(Farenheit, Celsius, "(x - 32) / 1.8")
+	NewConversion(Celsius, Fahrenheit, "x * 1.8 + 32")
+	NewConversion(Fahrenheit, Celsius, "(x - 32) / 1.8")
 	NewConversion(Celsius, Kelvin, "x + 273.15")
 	NewConversion(Kelvin, Celsius, "x - 273.15")
 }
